f45d429d47b7f8e06c2a9734207141891b0057fa
Ryan Bray <ryan.bray@slalom.com>
2023-04-19 10:02:14 -0600

    upgraded Vue version

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Configure your app here
+  devServer: {
+    port: 8080,
+    open: true,
+  },
+  // Other options...
+  configureWebpack: {
+    devtool: "source-map",
+  },
+};
